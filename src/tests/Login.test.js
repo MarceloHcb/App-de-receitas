dbd6d83e3c28ddb069983600870b9ae65e8ef686
MarceloHcb <marcelohcb@msn.com>
2023-03-02 16:52:52 -0300

    inicia testes de login e cria pastas redux e helpers, req1

---
+++ src/tests/Login.test.js
@@ -0,0 +1,11 @@
+describe('Testa a página de login', () => {
+  test('Verifica se a tela de login é renderizada na rota correta', () => {
+
+  });
+  test('Verifica se renderiza o comapo de login', () => {
+    expect().toBeInTheDocument();
+  });
+  test('', () => {
+
+  });
+});
