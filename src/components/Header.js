dd15732434d4338d5f4ec3d4a07e3cee57a55285
BrunoSouza <desouza88@hotmail.com>
2023-03-02 18:38:09 -0300

    Requisito 8 feito - falta teste

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
 
 function Header() {
   const history = useHistory();
 
   const page = history.location.pathname;
   const pageTitle = page === '/' ? '' : page.replace('/', '');
+
+  const handleClickToProfile = () => {
+    history.push('/profile');
+  };
+
   return (
     <header>
       <h1 data-testid="page-title">
@@ -13,11 +19,15 @@ function Header() {
           pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1)
         }
       </h1>
-      <img
-        src="/images/profileIcon.svg"
-        alt=""
-        data-testid="profile-top-btn"
-      />
+      <button
+        onClick={ handleClickToProfile }
+      >
+        <img
+          src={ profileIcon }
+          alt=""
+          data-testid="profile-top-btn"
+        />
+      </button>
     </header>
   );
 }
