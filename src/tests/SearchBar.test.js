c559dae3c453d95ed40c94c8b6ef6f7ac230db5a
BrunoSouza <desouza88@hotmail.com>
2023-03-10 17:30:07 -0300

    testes em andamento

--- src/tests/SearchBar.test.js
+++ src/tests/SearchBar.test.js
@@ -1,7 +1,9 @@
-import { act, screen } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { renderWithRouter } from '../helpers/renderWith';
 import Provider from '../context/Provider';
+import fetch from '../../cypress/mocks/fetch';
 
 // const filters = ['ingredient-search-radio', 'name-search-radio', 'first-letter-search-radio'];
 // const buttonSearch = 'search-top-btn';
@@ -10,12 +12,21 @@ import Provider from '../context/Provider';
 // const meal = {
 //   idMeal: 1,
 // };
+const searchButtonId = 'search-top-btn';
+const searchInputId = 'search-input';
+const submitId = 'exec-search-btn';
+const nameRadioId = 'name-search-radio';
+const firstLetterID = 'first-letter-search-radio';
+const ingredientRadioId = 'ingredient-search-radio';
 
 describe('Testa componente SearchBar', () => {
   beforeEach(() => {
     global.fetch = jest.fn(fetch);
   });
-  it('Verifica se o filtro é feito ao clicar na opção ingrediente após digitar o valor chicken', async () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('', () => {
     const { history } = renderWithRouter(
       <Provider>
         <App />
@@ -25,8 +36,191 @@ describe('Testa componente SearchBar', () => {
       history.push('/meals');
     });
 
-    const searchTopBtn = screen.getByTestId('search-top-btn');
+    const searchTopBtn = screen.getByTestId(searchButtonId);
     expect(searchTopBtn).toBeInTheDocument();
+  });
+  it('', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+
+    const searchTopBtn = screen.getByTestId(searchButtonId);
+    userEvent.click(searchTopBtn);
+
+    const searchBar = screen.getByTestId(submitId);
+    expect(searchBar).toBeInTheDocument();
+  });
+  it('Testando botão de ingrediente', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+
+    const searchTopBtn = screen.getByTestId(searchButtonId);
+    userEvent.click(searchTopBtn);
+
+    const searchInput = screen.getByTestId(searchInputId);
+    userEvent.type(searchInput, 'beef');
+
+    const ingredientRadioBtn = screen.getByTestId(ingredientRadioId);
+    userEvent.click(ingredientRadioBtn);
+
+    const searchBar = screen.getByTestId(submitId);
+    userEvent.click(searchBar);
+
+    await waitFor(() => {
+      const beefMustard = screen.getByTestId('0-recipe-card');
+      expect(beefMustard).toBeInTheDocument();
+    });
+  });
+  it('Testando global alert', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+
+    global.alert = jest.fn(() => {});
+
+    const searchTopBtn = screen.getByTestId(searchButtonId);
+    userEvent.click(searchTopBtn);
+
+    const searchInput = screen.getByTestId(searchInputId);
+    userEvent.type(searchInput, 'aa');
+
+    const firstLetterBtn = screen.getByTestId(firstLetterID);
+    userEvent.click(firstLetterBtn);
+
+    const searchBar = screen.getByTestId(submitId);
+    userEvent.click(searchBar);
+
+    await waitFor(() => {
+      expect(global.alert).toHaveBeenCalledWith('Your search must have only 1 (one) character');
+    });
+  });
+  it('Testando name input', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+
+    const searchTopBtn = screen.getByTestId(searchButtonId);
+    userEvent.click(searchTopBtn);
+
+    const searchInput = screen.getByTestId(searchInputId);
+    userEvent.type(searchInput, 'chicken');
+
+    const nameSearchBtn = screen.getByTestId(nameRadioId);
+    userEvent.click(nameSearchBtn);
+
+    const searchBar = screen.getByTestId(submitId);
+    userEvent.click(searchBar);
+
+    await waitFor(() => {
+      const chickenHandi = screen.getByTestId('0-recipe-card');
+      expect(chickenHandi).toBeInTheDocument();
+    });
+  });
+  it('Testando botão primeira letra', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    act(() => {
+      history.push('/meals');
+    });
+
+    const searchTopBtn = screen.getByTestId(searchButtonId);
+    userEvent.click(searchTopBtn);
+
+    const searchInput = screen.getByTestId(searchInputId);
+    userEvent.type(searchInput, 'aa');
+
+    const firstLetterBtn = screen.getByTestId(firstLetterID);
+    userEvent.click(firstLetterBtn);
+
+    const searchBar = screen.getByTestId(submitId);
+    userEvent.click(searchBar);
+
+    await waitFor(() => {
+      const appleCrumble = screen.getByTestId('1-recipe-card');
+      expect(appleCrumble).toBeInTheDocument();
+    });
+  });
+  it('', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    act(() => {
+      history.push('/drinks');
+    });
+
+    console.log(history.location.pathname);
+
+    const cocoaBtn = screen.getByTestId('page-title');
+    expect(cocoaBtn).toBeInTheDocument();
+
+    await waitFor(() => {
+      const cocktailBtn = screen.getByRole('button', {
+        name: /cocktail/i,
+      });
+      expect(cocktailBtn).toBeInTheDocument();
+
+      const ggDrink = screen.getByTestId('0-card-img');
+      expect(ggDrink).toBeInTheDocument();
+    });
+  });
+  it('', async () => {
+    const { history } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+    act(() => {
+      history.push('/drinks');
+    });
+
+    const searchTopBtn = screen.getByTestId(searchButtonId);
+    expect(searchTopBtn).toBeInTheDocument();
+    userEvent.click(searchTopBtn);
+
+    const searchInput = screen.getByTestId(searchInputId);
+    expect(searchInput).toBeInTheDocument();
+    userEvent.type(searchInput, 'water');
+
+    const nameSearchBtn = screen.getByTestId(nameRadioId);
+    expect(nameSearchBtn).toBeInTheDocument();
+    userEvent.click(nameSearchBtn);
+
+    const searchBar = screen.getByTestId(submitId);
+    expect(searchBar).toBeInTheDocument();
+    userEvent.click(searchBar);
+
+    console.log(history.location);
+
+    // await waitFor(() => {
+    //   const currentPath = history.location.pathname;
+    //   expect(currentPath).toBe('/drinks/178332');
+    // });
   });
 
 //   it('Verifica se o filtro é feito ao clicar na opção Name após digitar o valor soup', async () => {
