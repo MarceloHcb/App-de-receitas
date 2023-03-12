66c7eb225e6fa9ce50c2cc79e13d5bdbe645a591
MarceloHcb <marcelohcb@msn.com>
2023-03-12 06:14:39 -0300

    cria filtro por comida e bebida, redireciona para detalhes do objeto clicado, req47 48 49

--- src/components/DoneRecipesCard.js
+++ src/components/DoneRecipesCard.js
@@ -1,21 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import shareIcon from '../images/shareIcon.svg';
+import '../css/recipeinprogress.css';
 
-const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-console.log(doneRecipes);
+const data = JSON.parse(localStorage.getItem('doneRecipes'));
+console.log(data);
 
 function DoneRecipesCard() {
+  const [message, setMessage] = useState('');
+  const [doneRecipes, setRecipesDone] = useState(data);
+  const timeNumber = 3000;
+  if (message) {
+    setTimeout(() => setMessage(''), timeNumber);
+  }
+
+  const handleClick = (param) => {
+    if (param === 'All') {
+      setRecipesDone(data);
+    }
+    if (param === 'Meals') {
+      setRecipesDone(doneRecipes?.filter(({ type }) => type === 'meal'));
+    }
+    if (param === 'Drinks') {
+      setRecipesDone(doneRecipes?.filter(({ type }) => type === 'drink'));
+    }
+  };
+
   return (
     <div>
-      <button type="button" data-testid="filter-by-all-btn">All</button>
-      <button type="button" data-testid="filter-by-meal-btn">Meal</button>
-      <button type="button" data-testid="filter-by-drink-btn">Drink</button>
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+        onClick={ () => handleClick('All') }
+      >
+        All
+
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-meal-btn"
+        onClick={ () => handleClick('Meals') }
+      >
+        Meals
+
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+        onClick={ () => handleClick('Drinks') }
+      >
+        Drinks
+
+      </button>
       {doneRecipes?.map((el, index) => (
         <div key={ index }>
-          <h3 data-testid={ `${index}-horizontal-name` }>{el.name}</h3>
+          <Link to={ `/${el.type}s/${el.id}` }>
+            <h3 data-testid={ `${index}-horizontal-name` }>{el.name}</h3>
+            <img
+              src={ el.image }
+              className="img"
+              alt=""
+              data-testid={ `${index}-horizontal-image` }
+            />
+          </Link>
           {el.alcoholicOrNot
           && <h4 data-testid={ `${index}-horizontal-top-text` }>{el.alcoholicOrNot}</h4>}
-          <img src={ el.image } alt="" data-testid={ `${index}-horizontal-image` } />
           <h4 data-testid={ `${index}-horizontal-done-date` }>{el.doneDate}</h4>
           <h4 data-testid={ `${index}-horizontal-tags` }>{el.tags}</h4>
           <h4 data-testid={ `${index}-horizontal-top-text` }>
@@ -31,8 +80,12 @@ function DoneRecipesCard() {
 
           <button
             type="button"
-
+            onClick={ () => {
+              navigator.clipboard.writeText(`http://localhost:3000/${el.type}s/${el.id}`)
+                .then(() => setMessage('Link copied!'));
+            } }
           >
+            <p style={ { color: 'red' } }>{message}</p>
             <img
               src={ shareIcon }
               alt="share"
